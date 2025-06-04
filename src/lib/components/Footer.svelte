776d4dc940ec32da8ac6ca8d60883a06ebdc31f4
PilkeySEK <PilkeySEK@gmx.de>
2025-06-04 16:33:00 +0200

    Ubuntu Mono + GitHub in footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,13 @@
 <script>
-
 </script>
 
-<h1>footer</h1>
+<div class="border-t-2 border-solid border-blue-900/50 bg-black">
+	<div class="flex items-center p-3">
+        <span class="border-solid rounded-sm border-1 border-orange-500/25 bg-orange-500/10">
+		<a href="https://github.com/PilkeySEK" class="flex items-center gap-1 p-1">
+			<img src="/GitHub_logo_white.png" alt="GitHub logo" class="h-6 w-6" />
+			<p>GitHub</p>
+		</a>
+        </span>
+	</div>
+</div>
