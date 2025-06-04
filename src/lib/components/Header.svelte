83c020b0477f288e821751692308679d6f2ec3fa
PilkeySEK <PilkeySEK@gmx.de>
2025-06-04 15:16:17 +0200

    header+bg-color

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,3 +1,11 @@
-<div class="sticky top-0 overflow-auto">
-	<p>content</p>
+<script>
+</script>
+
+<div class="sticky top-0 overflow-auto bg-linear-to-r from-violet-500 to-pink-500 opacity-75 backdrop-blur-sm">
+	<b>
+		<span class="flex h-10 grid-cols-2 place-items-center gap-25 gap-x-10 pr-10 pl-10 text-white">
+			<a href="/">Home</a>
+			<a href="/about-me">About Me</a>
+		</span>
+	</b>
 </div>
