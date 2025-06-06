6fe7d2b0eba03afefc9ea696f534499bf92e8137
PilkeySEK <PilkeySEK@gmx.de>
2025-06-06 15:31:08 +0200

    basic Box component, needs improvement (image size)

---
+++ src/lib/components/Box1.svelte
@@ -0,0 +1,34 @@
+<script>
+	let {
+		title,
+		subtitle = undefined,
+		text,
+		footer = undefined,
+		image = undefined,
+		image_alt = undefined,
+		width = 0,
+		height = 0
+	} = $props();
+</script>
+
+<div
+	class="{width === 0 ? '' : `w-${width}`} {height === 0
+		? ''
+		: `w-${height}`} border-2 border-solid border-blue-500/20"
+>
+	<div class="flex">
+		{#if image}
+			<img src={image} alt={image_alt} />
+		{/if}
+		<div>
+			<b><p class="text-2xl">{title}</p></b>
+			{#if subtitle}
+				<b><p class="text-sm">{subtitle}</p></b>
+			{/if}
+			<p class="text-justify hyphens-auto">{text}</p>
+			{#if footer}
+				<p class="text-sm text-gray-500">{footer}</p>
+			{/if}
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
+<script>
+	import Box1 from "$lib/components/Box1.svelte";
+
+</script>
 <h1 style="text-align: center">h</h1>
-{#each Array(100) as i}
-    <br>
-{/each}
+<Box1 text="Hello aaahaaa" title="a" subtitle="abcdefg" footer="mmm" image="/assets/GitHub_logo_white_normalized.png" image_alt="h" />
