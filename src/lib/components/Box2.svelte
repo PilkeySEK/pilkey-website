31091f17818edcf88efebb34fce8c09e36b6fa2d
PilkeySEK <PilkeySEK@gmx.de>
2025-06-12 16:50:38 +0200

    better boxgit add .!

---
+++ src/lib/components/Box2.svelte
@@ -0,0 +1,27 @@
+<script>
+	let {
+		title = undefined,
+		subtitle = undefined,
+		text = undefined,
+		image = undefined,
+		image_alt = 'No alt provided.',
+		image_height = '100',
+		image_width = '100',
+		img_container_classes = ''
+	} = $props();
+</script>
+
+<div class="border-1 border-solid border-yellow-50">
+	<div class="flex flex-row flex-wrap md:flex-wrap lg:flex-nowrap text-wrap gap-5">
+		<div class={img_container_classes}>
+			<img src={image} alt={image_alt} width={image_width} height={image_height} class="" />
+		</div>
+		<div>
+			<p class="text-2xl"><b>{@html title}</b></p>
+			{#if subtitle}
+			<p class="text-gray-500">{@html subtitle}</p>
+			{/if}
+			<p>{@html text}</p>
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,25 @@
 <script>
-	import Box1 from "$lib/components/Box1.svelte";
-
+	// import Box1 from '$lib/components/Box1.svelte';
+	import Box2 from '$lib/components/Box2.svelte';
 </script>
+
 <h1 style="text-align: center">h</h1>
+<!--
 <Box1 text="Hello aaahaaa" title="a" subtitle="abcdefg" footer="mmm" image="/assets/GitHub_logo_white_normalized.png" image_alt="h" />
+<Box1 text="Hello aaahaaa" title="a" subtitle="abcdefg" footer="mmm" image="/assets/GitHub_logo_white_normalized.png" image_alt="h" />
+-->
+<div class="grid grid-cols-1 gap-10 p-10 md:grid-cols-1 lg:grid-cols-2">
+	<Box2
+		title="hi"
+		subtitle="asdf"
+		text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas placerat lacinia nibh, nec molestie quam."
+		image="/assets/GitHub_logo_white_normalized.png"
+		image_alt="abc"
+	/>
+	<Box2
+		title="hi"
+		text="normal text"
+		image="/assets/GitHub_logo_white_normalized.png"
+		image_alt="abc"
+	/>
+</div>
