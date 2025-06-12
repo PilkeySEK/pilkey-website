00494fbd15a6f30a8d6c2db8907bafcd21725e69
PilkeySEK <PilkeySEK@gmx.de>
2025-06-12 17:05:46 +0200

    remove img_container_classes

--- src/lib/components/Box2.svelte
+++ src/lib/components/Box2.svelte
@@ -6,16 +6,13 @@
 		image = undefined,
 		image_alt = 'No alt provided.',
 		image_height = '100',
-		image_width = '100',
-		img_container_classes = ''
+		image_width = '100'
 	} = $props();
 </script>
 
-<div class="border-1 border-solid border-yellow-50">
-	<div class="flex flex-row flex-wrap md:flex-wrap lg:flex-nowrap text-wrap gap-5">
-		<div class={img_container_classes}>
+<div class="border-2 border-solid border-blue-900/50 rounded-xl bg-blue-900/20">
+	<div class="flex flex-row flex-wrap md:flex-wrap lg:flex-nowrap text-wrap gap-5 p-1">
 			<img src={image} alt={image_alt} width={image_width} height={image_height} class="" />
-		</div>
 		<div>
 			<p class="text-2xl"><b>{@html title}</b></p>
 			{#if subtitle}
