404a23fdcc7cc37d7d2d189b9cbba68428354747
PilkeySEK <PilkeySEK@gmx.de>
2025-06-12 17:46:26 +0200

    a lot of stuff related to GridBox

--- src/lib/components/GridBox.svelte
+++ src/lib/components/GridBox.svelte
@@ -6,13 +6,14 @@
 		image = undefined,
 		image_alt = 'No alt provided.',
 		image_height = '100',
-		image_width = '100'
+		image_width = '100',
+		col_span = ""
 	} = $props();
 </script>
 
-<div class="border-2 border-solid border-blue-900/50 rounded-xl bg-blue-900/20">
+<div class="border-2 border-solid border-blue-900/50 rounded-xl bg-blue-900/20 {col_span}">
 	<div class="flex flex-row flex-wrap md:flex-wrap lg:flex-nowrap text-wrap gap-5 p-1">
-			<img src={image} alt={image_alt} width={image_width} height={image_height} class="" />
+			<img src={image} alt={image_alt} width={image_width} height={image_height} class="h-1/1 rounded-xl" />
 		<div>
 			<p class="text-2xl"><b>{@html title}</b></p>
 			{#if subtitle}
