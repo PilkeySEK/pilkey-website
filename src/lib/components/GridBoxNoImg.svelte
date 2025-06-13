f3569be05abc1b8f8ab27b091fb06c9f10a6aa7a
PilkeySEK <PilkeySEK@gmx.de>
2025-06-13 16:05:07 +0200

    GridBoxNoImg

---
+++ src/lib/components/GridBoxNoImg.svelte
@@ -0,0 +1,22 @@
+<script>
+	let {
+		title = undefined,
+		subtitle = undefined,
+		text = undefined,
+		image = undefined,
+		image_alt = 'No alt provided.',
+		image_height = '100',
+		image_width = '100',
+		col_span = ''
+	} = $props();
+</script>
+
+<div class="rounded-xl border-2 border-solid border-blue-900/50 bg-blue-900/20 {col_span}">
+	<div class="p-3">
+		<p class="text-2xl"><b>{@html title}</b></p>
+		{#if subtitle}
+			<p class="text-gray-500">{@html subtitle}</p>
+		{/if}
+		<p>{@html text}</p>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	// import Box1 from '$lib/components/Box1.svelte';
 	import GridBox from '$lib/components/GridBox.svelte';
+	import GridBoxNoImg from '$lib/components/GridBoxNoImg.svelte';
 </script>
 
 <div class="grid grid-cols-1 gap-10 p-10 md:grid-cols-1 lg:grid-cols-2">
@@ -13,5 +14,25 @@
 			<br>You should really check out my <a href='https://github.com/PilkeySEK'>GitHub</a> :3"
 			image="/assets/pilkey_pfp.png"
 			image_alt="Pilkey's picture"
+		/>
+		<GridBoxNoImg
+			title="Technology"
+			subtitle="What I use"
+			text="<b>== Hardware ==</b>
+			<dl>
+				<dt>- Lenovo IdeaPad C340-15IWL</dt>
+				<dd>- Upgraded to 12GB RAM, Intel i5-8265U (8) @ 3.9GHz, NVIDIA GeForce MX230</dd>
+				<dt>- LG ULTRAGEAR</dt>
+				<dd>- HD Resolution</dd>
+				<dt>- Samsung Galaxy A14 5G</dt>
+				<dd>- Certified Android enjoyer</dd>
+			</dl>
+			<b>== Software ==</b>
+			<dl>
+				<dt>- Linux Mint 22.1</dt>
+				<dd>- Cinnamon</dd>
+				<dt>- Visual Studio Code</dt>
+				<dt>- IntelliJ IDEA</dt>
+			</dl>"
 		/>
 </div>
