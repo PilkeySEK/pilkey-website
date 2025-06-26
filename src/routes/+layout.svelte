be8bca4b97aa5b4d25b4ed5a1e744436212fa42e
PilkeySEK <PilkeySEK@gmx.de>
2025-06-26 16:27:43 +0200

    switch from legacy to @render

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,13 @@
 	import "../app.css";
   import Header from "$lib/components/Header.svelte";
   import Footer from "$lib/components/Footer.svelte";
+  let { children } = $props();
 </script>
 
 <Header />
 
 <main>
-  <slot />
+  {@render children()}
 </main>
 
 <Footer />
